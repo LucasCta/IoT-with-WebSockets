ff629bb8d16c590720bf440431d9aa764049d005
LucasCta <lucas.ct.araujo@gmail.com>
2022-08-16 01:27:11 -0300

    WebSockets First Implementation

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
-const express = require('express'); 
-const { readFile } = require('fs').promises;
+import express, { response } from 'express'
+import {Server} from "socket.io"
+import {createServer} from 'http'
+import { readFile } from 'node:fs/promises'
 
-const app = express();
+const app = express(); 
+const server = createServer(app); 
+const sockets = new Server(server);
+
 app.use(express.static('public'));
 app.use(express.urlencoded({extended: true}));
+
+app.use(function (req, res, next) {
+    res.setHeader('Content-Type', 'text/html');
+    next();
+});
 
 app.get('/', async (req, res) => {
     res.send(await readFile('./main.html', 'utf8'));
@@ -14,4 +24,18 @@ app.post('/', (req, res) => {
     res.redirect('/');
 });
 
-app.listen(process.env.PORT || 3000, () => console.log(`http://localhost:3000`));
+sockets.on('connection', (socket) => {
+    console.log(`User Connected: ${socket.id}`);
+    socket.on('disconnect', () => {
+        console.log(`User Disconnected: ${socket.id}`);
+    })
+});
+
+setInterval(() => {
+    sockets.emit("blink");
+    console.log("Trying to blink led")
+}, 10000);
+
+server.listen(3000, () => {
+    console.log("http://localhost:3000");
+});
